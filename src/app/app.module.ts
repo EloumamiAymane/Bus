9a8fadfe5d7cbdae8a9609f46ba2683bfecb84a1
unknown <aymaneeloumami@gmail.com>
2023-07-12 12:34:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardItemsComponent } from './card-items/card-items.component';
+import { BusDetailsComponent } from './bus-details/bus-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/*import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';*/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    CardItemsComponent,
+    BusDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/app/app.js
@@ -0,0 +1,12 @@
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.style.display = (dropdown.style.display === "none") ? "block" : "none";
+}
+function toggleDropdown1() {
+  var dropdown = document.getElementById("myDropdown1");
+  dropdown.style.display = (dropdown.style.display === "none") ? "block" : "none";
+}
+function handle() {
+  var dropdown = document.getElementById("dropdown");
+  dropdown.style.display = (dropdown.style.display === "none") ? "block" : "none";
+}
